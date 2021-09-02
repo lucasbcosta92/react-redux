08349d6eedd81f187a4f25bb39803743821b9eba
lucas.costa <lucas.costa@acct.global>
2021-09-02 14:27:49 -0300

    feat: added rules for added product in cart

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,4 +1,5 @@
 import { Reducer } from "redux";
+import produce from "immer";
 import { ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
@@ -6,27 +7,32 @@ const INITIAL_STATE: ICartState = {
 };
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case "ADD_PRODUCT_TO_CART": {
-      const { product } = action.payload;
+  // produzindo um novo estado através de um estado anteiror
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case "ADD_PRODUCT_TO_CART": {
+        const { product } = action.payload;
 
-      return {
-        ...state,
-        items: [
-          ...state.items,
-          {
-            product: { ...product },
+        const productInCartIndex = draft.items.findIndex(
+          (item) => item.product.id === product.id
+        );
+
+        if (productInCartIndex >= 0) {
+          draft.items[productInCartIndex].quantity++;
+        } else {
+          draft.items.push({
+            product,
             quantity: 1,
-          },
-        ],
-      };
+          });
+        }
 
-      break;
+        break;
+      }
+      default: {
+        return draft;
+      }
     }
-    default: {
-      return state;
-    }
-  }
+  });
 };
 
 export default cart;
